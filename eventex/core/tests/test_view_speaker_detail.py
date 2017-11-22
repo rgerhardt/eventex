c99f91a3a4588802adfb68e7577d9124d7bcc3bf
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2017-11-22 17:23:25 -0200

    add speaker model

---
+++ eventex/core/tests/test_view_speaker_detail.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+from eventex.core.models import Speaker
+
+
+class SpeakerDetailGet(TestCase):
+    def setUp(self):
+        Speaker.objects.create(
+            name='Grace Hopper',
+            slug='grace-hopper',
+            photo='http://hbn.link/hopper-pic',
+            website='http://hbn.link/hopper-site',
+            description='Programadora e almirante.'
+        )
+        self.resp = self.client.get(r('speaker_detail', slug='grace-hopper'))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/speaker_detail.html')
+
+    def test_html(self):
+        contents = [
+            'Grace Hopper',
+            'Programadora e almirante.',
+            'http://hbn.link/hopper-pic',
+            'http://hbn.link/hopper-site'
+        ]
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.resp, expected)
+
+    def test_speaker_must_be_in_context(self):
+        speaker = self.resp.context['speaker']
+        self.assertIsInstance(speaker, Speaker)
+
+
+class SpeakerDetailNotFound(TestCase):
+    def test_not_found(self):
+        resp = self.client.get(r('speaker_detail', slug='not-found'))
+        self.assertEqual(404, resp.status_code)
