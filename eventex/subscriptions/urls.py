03ea67c9e927e0f6cac24f0ecc4c957938afbeca
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2018-01-28 13:00:11 -0200

    migrate to django 2

--- eventex/subscriptions/urls.py
+++ eventex/subscriptions/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from eventex.subscriptions.views import new, detail
 
+app_name = 'subscriptions'
+
 urlpatterns = [
     url(r'^$', new, name='new'),
     url(r'^(?P<pk>\d+)/$', detail, name='detail'),
